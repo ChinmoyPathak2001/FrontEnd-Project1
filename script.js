d90b69251b41714f9fe19fe29ead4de4e4fbfe2b
ChinmoyPathak2001 <pathak.work.2025@gmail.com>
2025-07-24 20:23:05 +0530

    added gsap

--- script.js
+++ script.js
@@ -5,5 +5,20 @@ const scroll = new LocomotiveScroll({
 
 //gsap to animate
 gsap.from(".nlink", {
-    stagger: .1
+    stagger: .2,
+    y: 10,
+    duration: 1,
+    ease: Power2,
+    opacity: 0
 })
+
+//shery js to animate images.
+Shery.textAnimate("#headings h1", {
+    
+    style: 1,
+    // y: 10,
+    // delay: 0.1,
+    // duration: 2,
+    ease: "cubic-bezier(0.23, 1. 0.320, 1)",
+    multiplier: 0.1,
+});
